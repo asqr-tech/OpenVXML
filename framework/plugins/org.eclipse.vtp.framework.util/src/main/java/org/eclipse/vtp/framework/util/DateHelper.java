70a4479e9e067e5c896159f4c6f456122df2b099
Sandeep Velagaleti <48468593+sv-mobigesture@users.noreply.github.com>
2019-09-06 11:33:22 +0530

    minor fixes related to orbit and nebula

--- framework/plugins/org.eclipse.vtp.framework.util/src/main/java/org/eclipse/vtp/framework/util/DateHelper.java
+++ framework/plugins/org.eclipse.vtp.framework.util/src/main/java/org/eclipse/vtp/framework/util/DateHelper.java
@@ -5,28 +5,16 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 
-public class DateHelper
-{
-	private static String[] datePatterns = new String[] { "M/d/yyyy",
-														  "M-d-yyyy",
-														  "M.d.yyyy"};
-	private static String[] timePatterns = new String[] { "h:mm:ss a z",
-														  "H:mm:ss z",
-														  "h:mm:ss a",
-														  "H:mm:ss",
-														  "h:mm a z",
-														  "H:mm z",
-														  "h:mm a",
-														  "H:mm"};
-	
-	public static Calendar parseDate(String dateString)
-	{
+public class DateHelper {
+	private static String[] datePatterns = new String[] { "M/d/yyyy", "M-d-yyyy", "M.d.yyyy" };
+	private static String[] timePatterns = new String[] { "h:mm:ss a z", "H:mm:ss z", "h:mm:ss a", "H:mm:ss",
+			"h:mm a z", "H:mm z", "h:mm a", "H:mm" };
+
+	public static Calendar parseDate(String dateString) {
 		Calendar cal = parseDate0(dateString);
-		if(cal != null)
-		{
+		if (cal != null) {
 			int index = dateString.indexOf("GMT");
-			if(index >= 0)
-			{
+			if (index >= 0) {
 				String tzOffsetString = dateString.substring(index);
 				TimeZone tzOffset = TimeZone.getTimeZone(tzOffsetString);
 				cal.setTimeZone(tzOffset);
@@ -34,53 +22,38 @@ public class DateHelper
 		}
 		return cal;
 	}
-	
-	private static Calendar parseDate0(String dateString)
-	{
-		for(String datePattern : datePatterns)
-		{
-			for(String timePattern : timePatterns)
-			{
+
+	private static Calendar parseDate0(String dateString) {
+		for (String datePattern : datePatterns) {
+			for (String timePattern : timePatterns) {
 				SimpleDateFormat sdf = new SimpleDateFormat(datePattern + " " + timePattern);
-				try
-				{
+				try {
 					sdf.parse(dateString);
 					return sdf.getCalendar();
-				}
-				catch (ParseException e)
-				{
+				} catch (ParseException e) {
 				}
 			}
 		}
-		for(String datePattern : datePatterns)
-		{
+		for (String datePattern : datePatterns) {
 			SimpleDateFormat sdf = new SimpleDateFormat(datePattern);
-			try
-			{
+			try {
 				sdf.parse(dateString);
 				return sdf.getCalendar();
-			}
-			catch (ParseException e)
-			{
+			} catch (ParseException e) {
 			}
 		}
-		for(String timePattern : timePatterns)
-		{
+		for (String timePattern : timePatterns) {
 			SimpleDateFormat sdf = new SimpleDateFormat(timePattern);
-			try
-			{
+			try {
 				sdf.parse(dateString);
 				return sdf.getCalendar();
-			}
-			catch (ParseException e)
-			{
+			} catch (ParseException e) {
 			}
 		}
 		return null;
 	}
-	
-	public static String toDateString(Calendar cal)
-	{
+
+	public static String toDateString(Calendar cal) {
 		SimpleDateFormat sdf = new SimpleDateFormat(datePatterns[0] + " " + timePatterns[0]);
 		sdf.setTimeZone(cal.getTimeZone());
 		return sdf.format(cal.getTime());
